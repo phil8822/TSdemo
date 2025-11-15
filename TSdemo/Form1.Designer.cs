aa187f4b1a40f4f0dfb1fe947c927163438a3a23
phil8822 <52148606+phil8822@users.noreply.github.com>
2025-11-15 09:39:21 -0800

    Added labels on Form1

--- TSdemo/Form1.Designer.cs
+++ TSdemo/Form1.Designer.cs
@@ -31,6 +31,8 @@
             GoButton = new Button();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // GoButton
@@ -44,7 +46,7 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(313, 119);
+            textBox1.Location = new Point(297, 179);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.ScrollBars = ScrollBars.Both;
@@ -53,18 +55,38 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(313, 539);
+            textBox2.Location = new Point(297, 691);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
             textBox2.ScrollBars = ScrollBars.Both;
             textBox2.Size = new Size(524, 386);
             textBox2.TabIndex = 2;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(297, 111);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 32);
+            label1.TabIndex = 3;
+            label1.Text = "Raw Json";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(297, 628);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 32);
+            label2.TabIndex = 4;
+            label2.Text = "Parsed";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1972, 1468);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(GoButton);
@@ -79,5 +101,7 @@
         private Button GoButton;
         private TextBox textBox1;
         private TextBox textBox2;
+        private Label label1;
+        private Label label2;
     }
 }
